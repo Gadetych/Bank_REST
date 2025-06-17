848084a2eed5b3182b6b6e027de2141c3182655c
Vladislav Gadetov <vlad-nijakun@mail.ru>
2025-06-17 12:27:40 +0300

    fix: привести в соответствие миграции и сущности User, Table

--- src/main/java/com/example/bankcards/entity/User.java
+++ src/main/java/com/example/bankcards/entity/User.java
@@ -13,13 +13,12 @@ import java.util.Collection;
 import java.util.List;
 
 @Entity
-@Table
+@Table(name = "users")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class User implements UserDetails {
-
     @Id
     @Column
     @GeneratedValue(strategy = GenerationType.IDENTITY)
