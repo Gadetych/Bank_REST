b8442f392e806fc20c830c6d34cc93b913a973c9
Vladislav Gadetov <vlad-nijakun@mail.ru>
2025-06-22 16:52:43 +0300

    feat: реализовать контроллер аутентификации

---
+++ src/main/java/com/example/bankcards/controller/AuthenticationController.java
@@ -0,0 +1,44 @@
+package com.example.bankcards.controller;
+
+import com.example.bankcards.dto.AuthenticationResponseDto;
+import com.example.bankcards.dto.LoginRequestDto;
+import com.example.bankcards.dto.RegistrationRequestDto;
+import com.example.bankcards.service.AuthenticationService;
+import com.example.bankcards.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+    private final UserService userService;
+
+
+    @PostMapping("/registration")
+    public ResponseEntity<String> register(@RequestBody RegistrationRequestDto registrationDto) {
+        if(userService.existsByUsername(registrationDto.getUsername())) {
+            return ResponseEntity.badRequest().body("Имя пользователя уже занято");
+        }
+        if(userService.existsByEmail(registrationDto.getEmail())) {
+            return ResponseEntity.badRequest().body("Email уже занят");
+        }
+        authenticationService.register(registrationDto);
+        return ResponseEntity.ok("Регистрация прошла успешно");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponseDto> authenticate(@RequestBody LoginRequestDto request) {
+        return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
+
+    @PostMapping("/refresh_token")
+    public ResponseEntity<AuthenticationResponseDto> refreshToken(HttpServletRequest request, HttpServletResponse response) {
+        return authenticationService.refreshToken(request, response);
+    }
+}
