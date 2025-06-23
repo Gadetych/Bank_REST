52b00107e9d869921f5fa8b8e80a44a26c51666d
Vladislav Gadetov <vlad-nijakun@mail.ru>
2025-06-23 21:32:47 +0300

    feat: добавить основные сущности карты, переводы

---
+++ src/main/java/com/example/bankcards/entity/BankCard.java
@@ -0,0 +1,45 @@
+package com.example.bankcards.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "bank_cards")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+public class BankCard {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "card_number", unique = true, nullable = false)
+    private String cardNumber;
+    @ManyToOne
+    @JoinColumn(name = "owner_id", nullable = false)
+    private User owner;
+    @Column(name = "expiration_date", nullable = false)
+    private LocalDate expirationDate;
+    @Column(name = "card_status", nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private CardStatus cardStatus;
+    @Column(name = "balance", nullable = false)
+    private BigDecimal balance;
+}
